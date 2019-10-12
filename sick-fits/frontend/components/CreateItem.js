f74a0f5de55c8bd9de910a241645ebea41766bac
Robert <robert@Roberts-MacBook-Pro.local>
2019-10-12 15:32:01 -0600

    added photo upload capability

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -42,6 +42,23 @@ class CreateItem extends Component {
                 this.setState({ [name]: val });
         };
 
+        uploadFile = async event => {
+                const { files } = event.target;
+                const data = new FormData();
+                data.append('file', files[0]);
+                data.append('upload_preset', 'sickfits');
+
+                const response = await fetch('https://api.cloudinary.com/v1_1/webdevdad/image/upload', {
+                        method: 'POST',
+                        body: data,
+                });
+                const file = await response.json();
+                this.setState({
+                        image: file.secure_url,
+                        largeImage: file.eager[0].secure_url,
+                });
+        };
+
         render() {
                 return (
                         <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -58,6 +75,18 @@ class CreateItem extends Component {
                                         >
                                                 <Error error={error} />
                                                 <fieldset disabled={loading} aria-busy={loading}>
+                                                        <label htmlFor="file">
+                                                                Image
+                                                                <input
+                                                                        type="file"
+                                                                        id="file"
+                                                                        name="file"
+                                                                        placeholder="Upload an image"
+                                                                        onChange={this.uploadFile}
+                                                                />
+                                                                {this.state.image && <img width='200' src= {this.state.image} alt='Upload Preview'/>}
+                                                        </label>
+
                                                         <label htmlFor="title">
                                                                 Title
                                                                 <input
